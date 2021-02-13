f0293ac822e41c3cf70877e1a95d341a54264658
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-13 16:32:03 +0800

    First Commit

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a sample Python Web App running on Flask Framework!"
+    return "Hello, this code is updated and pushed to Azure DevOps repo!"
