191ed4e7d955817bd19da4b299a2eb97c42a7b73
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-13 17:22:12 +0800

    Update and rename Shell from Bash.bsh to Shell from Bash.sh

---
+++ PowerShell/Shell from Bash.sh
@@ -0,0 +1,17 @@
+git status
+
+git add -a
+
+git status
+
+git commit -m "Commit-1"
+
+git status
+
+git remote -v
+
+git remote set-url origin https://SaritProject@dev.azure.com/SaritProject/PythonWebApp/_git/PythonWebApp
+
+git remote -v
+
+git push -u origin main
